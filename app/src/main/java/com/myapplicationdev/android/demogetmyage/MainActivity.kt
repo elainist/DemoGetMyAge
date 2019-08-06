b2bd9745727fee5f632ae93187b8d6a05e8ffea6
17010489 <elainenkx@gmail.com>
2019-08-06 11:53:31 +0800

    Initial commit

---
+++ app/src/main/java/com/myapplicationdev/android/demogetmyage/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.myapplicationdev.android.demogetmyage
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnGetAge.setOnClickListener {
+            val userDOB = etDOB.text.toString()
+            if (userDOB.isNotEmpty()) {
+                val year = Calendar.getInstance().get(Calendar.YEAR)
+                val age = year - userDOB.toInt()
+                var output = ""
+                when (age) {
+                    in 0..17 -> output = "Underage"
+                    in 18..65 -> output = "Young people"
+                    in 66..79 -> output = "Middle-aged"
+                    in 80..99 -> output = "Elderly"
+                    else -> {
+                        output = "Long-lived elderly"
+                    }
+
+                }
+
+           //     if (age < 18) {
+             //       output = "Underage"
+               // }else if (age in 18 .. 66){
+                 //   output = "Young People"
+                //}else if (age in 66 .. 80) {
+                    //  output = "Middle-Aged"
+              //  }else if (age in 80 .. 100) {
+                //    output = "Elderly"
+               // }else{
+                 //   output = "Long lived Elderly"
+                //}
+                tvShowAge.text = "Your age is $age" +
+                        "$output"
+
+            } else {
+                Toast.makeText(this, "Please enter your birth year", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
